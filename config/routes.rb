a533f70a0e21c00e6d5a104bc4dbeaecf6e80298
seiya-yano <seyaseyaseiya@gmail.com>
2020-02-02 19:56:11 +0900

    レビュー後修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "home#index"
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
@@ -6,5 +7,4 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
-  root to: "home#index"
 end
