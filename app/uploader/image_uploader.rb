2ff71c02095ad2189584c623338db974d0d1267b
YourName <you@example.com>
2020-02-12 16:54:00 +0900

    Update image_uploader.rb

--- app/uploader/image_uploader.rb
+++ app/uploader/image_uploader.rb
@@ -1,12 +1,13 @@
 # Choose what kind of storage to use for this uploader:
 class ImageUploader < CarrierWave::Uploader::Base
 
-if Rails.env.development? || Rails.env.test?
-  storage :file
-else
-  storage :fog
-end
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
-def store_dir
-  "uploads/# {model.class.to_s.underscore}/# {mounted_as}/# {model.id}"
+  def store_dir
+    "uploads/# {model.class.to_s.underscore}/# {mounted_as}/# {model.id}"
+  end
 end
