fb6416768078a2c037cc8310c0e965b24e93ab45
YourName <you@example.com>
2020-02-26 21:48:27 +0900

    購入完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   root to: "home#index"
 
-  get 'items' => 'items#index'
+  get 'items/index' => 'items#index'
+  get 'items/show' => 'items#show'
   get '/conform_user/:user_id' => 'users#conform_user', as: 'conform_user'
   get '/logout/:user_id' => 'users#logout', as: 'logout'
   get '/conform_card/:user_id' => 'users#conform_card', as: 'conform_card'
   get '/edit_profile/:user_id' => 'users#edit_profile', as: 'edit_profile'
   get '/mypage/:user_id' => 'users#mypage', as: 'mypage'
-
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :users, controllers: {
