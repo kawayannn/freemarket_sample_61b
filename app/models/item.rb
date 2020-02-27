5257ed1896abeacca0161a3a49d1a57c8b3fae8a
Kawata Seiya <kawataseiya@yahoo.co.jp>
2020-02-27 21:34:46 +0900

    item.rb アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  belongs_to_active_hash :prefecture
+  belongs_to :seller, class_name: "User"
+  belongs_to :buyer, class_name: "User"
+  belongs_to :brand
+  belongs_to :size
+  belongs_to :category
+  has_many :comments, dependent: :destroy
+  has_many :images, dependent: :destroy
+  has_many :likes, dependent: :destroy
 end
