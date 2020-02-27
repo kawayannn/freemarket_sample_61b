7d25dcaf4dd03cffef5d8f306fb73f0c589e2d90
Kawata Seiya <kawataseiya@yahoo.co.jp>
2020-02-27 21:44:48 +0900

    アイテム作成時のパラムスを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,12 @@ class ItemsController < ApplicationController
   end
   
   def new
+    @item = Item.new
+  end
+
+  private
+  def new_item_params
+    params.require(:item).permit(:name, :price, :postage, :description, :prefecture_id, :buyer_id, :brand_id, :size_id).merge(:seller_id: current_user.id)
   end
 
 end
