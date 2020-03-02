2f487ef38bb4d94442fc9ed17f6a09eba1b2a109
YourName <you@example.com>
2020-03-02 20:54:18 +0900

    確認４

--- db/migrate/20200218164446_create_addresses.rb
+++ db/migrate/20200218164446_create_addresses.rb
@@ -1,12 +1,12 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-      # t.integer       :zip_code,       null: false,  unique: true
-      # t.integer       :prefecture_id,  null: false
-      # t.string        :city,           null: false
-      # t.string        :street_name,    null: false, unique: true
-      # t.string        :building_name
-      # t.integer       :user_id,           foreign_key: true
+      t.integer       :zip_code,       null: false,  unique: true
+      t.integer       :prefecture_id,  null: false
+      t.string        :city,           null: false
+      t.string        :street_name,    null: false, unique: true
+      t.string        :building_name
+      t.references    :user,           foreign_key: true
       t.timestamps
     end
   end
