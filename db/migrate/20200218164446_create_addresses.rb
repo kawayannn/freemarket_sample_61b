ddf1632b01651258b14893388f7594115e2da403
YourName <you@example.com>
2020-03-02 20:23:05 +0900

    確認

--- db/migrate/20200218164446_create_addresses.rb
+++ db/migrate/20200218164446_create_addresses.rb
@@ -6,7 +6,7 @@ class CreateAddresses < ActiveRecord::Migration[5.2]
       t.string        :city,           null: false
       t.string        :street_name,    null: false, unique: true
       t.string        :building_name
-      t.references    :user,           foreign_key: true
+      t.integer       :user_id,           foreign_key: true
       t.timestamps
     end
   end
