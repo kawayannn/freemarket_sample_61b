7578744b876682102d7102b83ad80d5300dfdbc6
seiya-yano <seyaseyaseiya@gmail.com>
2020-03-03 18:10:26 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   
   
 
-  resources :items, only:[:show,:new,buy_check]
+  resources :items, only:[:show,:new,:buy_check]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
