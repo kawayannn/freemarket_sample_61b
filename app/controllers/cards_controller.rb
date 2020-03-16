0122ce78c27ee7be5a9a824e4398b05126861afa
Kawata Seiya <kawataseiya@yahoo.co.jp>
2020-03-16 21:00:01 +0900

    ユーザーのカード情報に合った物を表示

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,7 +3,28 @@ class CardsController < ApplicationController
 
   def index
     card = Card.where(user_id: current_user.id).first
-    redirect_to action: "new" if card.nil?
+    if card.nil?
+      redirect_to action: "new"
+    else
+      Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_SECRET_KEY]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @card = customer.cards.retrieve(card.card_id)
+      card_brand = @card.brand      
+      case card_brand
+      when "Visa"
+        @card_src = "visa.svg"
+      when "JCB"
+        @card_src = "jcb.svg"
+      when "MasterCard"
+        @card_src = "master-card.svg"
+      when "American Express"
+        @card_src = "american_express.svg"
+      when "Diners Club"
+        @card_src = "dinersclub.svg"
+      when "Discover"
+        @card_src = "discover.svg"
+      end
+    end
   end
 
   def new
