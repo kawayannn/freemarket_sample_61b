af56e10ebca3a8993370e206cc4a37cafa962aa2
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-03-17 21:32:32 +0900

    不要なコメントアウトを削除

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -29,16 +29,4 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-  # def authorization
-  #   sns_info = User.from_omniauth(request.env["omniauth.auth"])
-  #   @user = sns_info[:user]
-
-  #   if @user.persisted?
-  #     sign_in_and_redirect @user, event: :authentication
-  #   else
-  #     @sns_id = sns_info[:sns].id
-  #     render template: 'devise/registrations/new'
-  #   end
-  # end
-
 end
