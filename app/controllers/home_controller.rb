8e464858c2156dcfbff4ac7659ae34ef4db0601d
takumivvvqqq <57716986+takumivvvqqq@users.noreply.github.com>
2020-03-22 22:47:35 +0900

    flex

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,8 @@ class HomeController < ApplicationController
   end
 
   def index
-    @lady_item = Item.where(category_id:1).order(category_id: 'DESC').limit(16)
-    # @man_item = Item.where(category_id:2).order(:id 'DESC').limit(10)
-    # @category= Category.all
+    @lady_item = Item.all.order(category_id: 'DESC').limit(16)
+    @man_item = Item.all.order(category_id: 'DESC').limit(16)
   end
 
   
